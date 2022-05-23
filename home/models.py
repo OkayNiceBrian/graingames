81cddcd3c75ec400984369f22b6c0c80531fbb44
Brian Jackson <brianjackson1224@gmail.com>
2022-05-22 21:29:07 -0700

    Added game model

--- home/models.py
+++ home/models.py
@@ -7,3 +7,12 @@ class User(models.Model):
     joined_date = models.DateTimeField('date joined')
     def __str__(self):
         return self.username
+    
+class Game(models.Model):
+    title = models.CharField(max_length=80)
+    description = models.CharField(max_length=5000)
+    link = models.CharField(max_length=255)
+    date_published = models.DateTimeField('date published')
+    created_by_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
